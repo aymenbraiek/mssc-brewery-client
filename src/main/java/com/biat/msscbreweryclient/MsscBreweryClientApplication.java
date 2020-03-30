b935ad132439621a77ed83778c05e053b4e7cba4
aymenbraiek <aymen.braiek@esprit.tn>
2020-03-30 15:04:28 +0100

    RestTemplate how create client for my service

---
+++ src/main/java/com/biat/msscbreweryclient/MsscBreweryClientApplication.java
@@ -0,0 +1,14 @@
+package com.biat.msscbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryClientApplication.class, args);
+		System.out.println("bonjour aymen");
+	}
+
+}
